32d8870ff8e1fef4978b9fc825a22190d9bdca54
PMorais13 <phmscout@gmail.com>
2021-06-10 01:47:14 -0300

    add estilo card

--- features-features-module.js
+++ features-features-module.js
@@ -13,13 +13,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/common */ "ofXK");
 /* harmony import */ var _features_routing_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./features-routing.module */ "0e1J");
 /* harmony import */ var _features_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./features.component */ "mXFZ");
-/* harmony import */ var _components_card_profile_card_profile_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/card-profile/card-profile.component */ "Pfh/");
-/* harmony import */ var _components_projects_projects_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./components/projects/projects.component */ "kg6i");
-/* harmony import */ var _components_formation_formation_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./components/formation/formation.component */ "/uTe");
-/* harmony import */ var _components_experience_experience_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./components/experience/experience.component */ "YDOH");
-/* harmony import */ var _components_contact_contact_component__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/contact/contact.component */ "cWEi");
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! @angular/core */ "fXoL");
-
+/* harmony import */ var _components_projects_projects_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/projects/projects.component */ "kg6i");
+/* harmony import */ var _components_formation_formation_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./components/formation/formation.component */ "/uTe");
+/* harmony import */ var _components_experience_experience_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./components/experience/experience.component */ "YDOH");
+/* harmony import */ var _components_contact_contact_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./components/contact/contact.component */ "cWEi");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @angular/core */ "fXoL");
 
 
 
@@ -31,17 +29,16 @@ __webpack_require__.r(__webpack_exports__);
 class FeaturesModule {
 }
 FeaturesModule.ɵfac = function FeaturesModule_Factory(t) { return new (t || FeaturesModule)(); };
-FeaturesModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_8__["ɵɵdefineNgModule"]({ type: FeaturesModule });
-FeaturesModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_8__["ɵɵdefineInjector"]({ imports: [[
+FeaturesModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_7__["ɵɵdefineNgModule"]({ type: FeaturesModule });
+FeaturesModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_7__["ɵɵdefineInjector"]({ imports: [[
             _angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"],
             _features_routing_module__WEBPACK_IMPORTED_MODULE_1__["FeaturesRoutingModule"],
         ]] });
-(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_8__["ɵɵsetNgModuleScope"](FeaturesModule, { declarations: [_features_component__WEBPACK_IMPORTED_MODULE_2__["FeaturesComponent"],
-        _components_card_profile_card_profile_component__WEBPACK_IMPORTED_MODULE_3__["CardProfileComponent"],
-        _components_projects_projects_component__WEBPACK_IMPORTED_MODULE_4__["ProjectsComponent"],
-        _components_formation_formation_component__WEBPACK_IMPORTED_MODULE_5__["FormationComponent"],
-        _components_experience_experience_component__WEBPACK_IMPORTED_MODULE_6__["ExperienceComponent"],
-        _components_contact_contact_component__WEBPACK_IMPORTED_MODULE_7__["ContactComponent"]], imports: [_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"],
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_7__["ɵɵsetNgModuleScope"](FeaturesModule, { declarations: [_features_component__WEBPACK_IMPORTED_MODULE_2__["FeaturesComponent"],
+        _components_projects_projects_component__WEBPACK_IMPORTED_MODULE_3__["ProjectsComponent"],
+        _components_formation_formation_component__WEBPACK_IMPORTED_MODULE_4__["FormationComponent"],
+        _components_experience_experience_component__WEBPACK_IMPORTED_MODULE_5__["ExperienceComponent"],
+        _components_contact_contact_component__WEBPACK_IMPORTED_MODULE_6__["ContactComponent"]], imports: [_angular_common__WEBPACK_IMPORTED_MODULE_0__["CommonModule"],
         _features_routing_module__WEBPACK_IMPORTED_MODULE_1__["FeaturesRoutingModule"]], exports: [_features_component__WEBPACK_IMPORTED_MODULE_2__["FeaturesComponent"]] }); })();
 
 
