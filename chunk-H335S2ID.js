d184033db25b669182dd9441d3e465f6e95c6a6e
Paulo Morais <phmscout@gmail.com>
2026-03-18 18:29:36 -0300

    feat: novo site

---
+++ chunk-H335S2ID.js
@@ -0,0 +1,1 @@
+var r=new Intl.NumberFormat("pt-BR",{style:"currency",currency:"BRL"}),e=new Intl.DateTimeFormat("pt-BR"),n=t=>r.format(t),o=t=>e.format(new Date(t));export{n as a,o as b};
--- chunk-QDXT4NTO.js
+++
@@ -1,1 +0,0 @@
-function e(r){return r.normalize("NFD").replace(/[^\w\s-]/g,"").trim().toLowerCase().replace(/\s+/g,"-")}export{e as a};
